6a66e941d8aed2a4c503174d0427c76737774d56
HaseebIshaq <118919974+HaseebIshaq@users.noreply.github.com>
2024-07-12 17:56:48 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,9 @@
 provider "aws" {
-  region = "us-east-1"  # Replace with your desired AWS region
+  region = "us-east-1"
 }
 
-resource "aws_instance" "my_ec2_instance" {
-  ami           = "ami-0b72821e2f351e396"  # Replace with your desired AMI ID
-  instance_type = "t2.micro"  # Replace with your desired instance type
-  subnet_id     = subnet-054cb907a936367bf # Replace with your subnet ID
-
-  key_name      = "my-key-pair"  # This will be created by Ansible
-
-  tags = {
-    Name = "MyEC2Instance"
-  }
-
-  provisioner "local-exec" {
-    command = "ansible-playbook -i '${self.public_ip},' ../ansible/playbook.yml"
-    environment = {
-      ANSIBLE_HOST_KEY_CHECKING = "False"
-    }
-  }
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_subnet" "my_subnet" {
@@ -26,6 +11,15 @@ resource "aws_subnet" "my_subnet" {
   cidr_block = "10.0.1.0/24"
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_instance" "my_ec2_instance" {
+  ami           = "ami-0b72821e2f351e396"  # Replace with your desired AMI ID
+  instance_type = "t2.micro"
+  subnet_id     = subnet-054cb907a936367bf
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i '18.206.125.130' ../ansible/playbook.yml"
+    environment = {
+      ANSIBLE_HOST_KEY_CHECKING = "False"  # Disable host key checking for simplicity (not recommended for production)
+    }
+  }
 }
